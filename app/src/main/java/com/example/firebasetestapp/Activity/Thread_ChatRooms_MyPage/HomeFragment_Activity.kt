dd36139dc3ef2fe810a719c8d6260a918d87ea8a
yss_miura <jane.was.gentleman@gmail.com>
2020-11-16 21:43:41 +0900

    push通知

--- app/src/main/java/com/example/firebasetestapp/Activity/Thread_ChatRooms_MyPage/HomeFragment_Activity.kt
+++ app/src/main/java/com/example/firebasetestapp/Activity/Thread_ChatRooms_MyPage/HomeFragment_Activity.kt
@@ -2,17 +2,22 @@
 package com.example.firebasetestapp.Activity.Thread_ChatRooms_MyPage
 
 import android.app.Activity
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import com.afollestad.materialdialogs.MaterialDialog
 import com.example.firebasetestapp.R
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main_thread_.*
+import okhttp3.*
+import java.io.IOException
 
 class HomeFragment_Activity : AppCompatActivity() {
+
 
 //    private val fragmentList = arrayListOf(
 //        Thread_Fragment(),
@@ -28,6 +33,9 @@ class HomeFragment_Activity : AppCompatActivity() {
 //        mainThead_ViewPager.addOnPageChangeListener()
         supportActionBar?.hide()
         setTabLayout()
+        sendPushButton.setOnClickListener {
+            sendPush()
+        }
     }
     // Fragmentの設定と、タブの設定
     private fun setTabLayout() {
@@ -61,7 +69,25 @@ class HomeFragment_Activity : AppCompatActivity() {
         progressDialog = null
     }
 
+    private fun sendPush(fcmToken: String = TARGET_FCM_TOKEN) {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://fcm.googleapis.com/fcm/send")
+            .addHeader("Authorization", "key=AAAAYoCkPaY:APA91bFAxP61YE38Sc33dA8uVCzmU0FtIKzNdTjk5LUXkhPkQneTFGjKHpcqlQyaXHPfFkcE7SVVNo0pDZms4fmfIT7reBFopEnPimgvz4229howecP8kmDj2pMfE4UMXXIh0x3XUN-O")
+            .post(RequestBody.create(MediaType.parse("application/json"), "{\"to\":\"dG0RiRWNQxiqlqhwriZeWP:APA91bG-qh9TolH3GXBNoI-ggYFxPlIgdD-7zoWKiKAR4i6cDyCH9-A77BFpbNckpNmI9jDy5uXDjg6kixZmNwrSmcsx6B-AQm0ZCse6ppqT_iMuSSjTpJzAbRNc3ESZFvj0n7jmIi8G\",\"data\":{\"name\":\"123546\",\"message\":\"123456\",\"roomId\":\"あいうえお\"}}"))
+            .build()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("FCM_Custom", "失敗")
+            }
+            override fun onResponse(call: Call, response: Response) {
+                Log.e("FCM_Custom", "成功")
+            }
+        })
+    }
+
     companion object {
+        private const val TARGET_FCM_TOKEN = "dG0RiRWNQxiqlqhwriZeWP:APA91bG-qh9TolH3GXBNoI-ggYFxPlIgdD-7zoWKiKAR4i6cDyCH9-A77BFpbNckpNmI9jDy5uXDjg6kixZmNwrSmcsx6B-AQm0ZCse6ppqT_iMuSSjTpJzAbRNc3ESZFvj0n7jmIi8G"
         fun start(context: Context) {
             val intent = Intent(context, HomeFragment_Activity::class.java)
             context.startActivity(intent)
--- app/src/main/java/com/example/firebasetestapp/service/MyFireBaseMessageCloudService.kt
+++ app/src/main/java/com/example/firebasetestapp/service/MyFireBaseMessageCloudService.kt
@@ -1,19 +1,26 @@
 package com.example.firebasetestapp.service
 
+import android.annotation.TargetApi
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.firebasetestapp.R
+import com.example.firebasetestapp.SplashActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MyFireBaseMessageCloudService: FirebaseMessagingService(){
+
+    private val notificationManager by lazy { applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
 
     override fun onNewToken(token: String) {
         // 端末＋アプリを一意に識別するためのトークンを取得
@@ -24,23 +31,126 @@ class MyFireBaseMessageCloudService: FirebaseMessagingService(){
 
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        showNotification(remoteMessage)
+        Log.w("FCM_Custom", "onMessageReceived remoteMessage:$remoteMessage")
+        showNotificationFromData(remoteMessage)
+//
+//        remoteMessage.let { message ->
+//            // 通知メッセージ
+//            message.notification?.let {
+//                // 通知メッセージを処理
+//                Log.w("push", "通知notification")
+//
+//            }
+//
+//            // データメッセージ
+//            message.data?.let {
+//                // データメッセージを処理
+//                Log.w("push", "通知data")
+//
+//            }
+//        }
+    }
 
-        remoteMessage.let { message ->
-            // 通知メッセージ
-            message.notification?.let {
-                // 通知メッセージを処理
-                Log.w("push", "通知notification")
+    private fun showNotificationFromData(remoteMessage: RemoteMessage) {
+        val data = remoteMessage.data
+        val title = data["name"] ?: "たいとる"
+        val body = data["message"] ?: "ぼでい"
+        val roomId = data["roomId"] ?: ""
+        val roomNotifyId = data["roomNotifyId"] ?: "0"
+        var tempId = 0
+        try {
+            tempId = roomNotifyId.toInt()
+        } catch(e: Exception) {}
 
+        Log.w("FCM_Custom", "showNotificationFromData title:$title body:$body")
+        showFcmNotification(title, body, null)
+    }
+
+
+    /**
+     * Fcmの通知を表示
+     *
+     * @param title
+     * @param message
+     * @param redirectType
+     * @param contentId
+     * @param imageUrl
+     */
+    fun showFcmNotification(title: String, message: String, imageUrl: String?) {
+        val intent = Intent(applicationContext, SplashActivity::class.java).putExtra("moveToRoom", "roomId")
+        val tempTitle = if (title.isNotEmpty()) title else applicationContext.getString(R.string.app_name)
+//        if (imageUrl == null)
+            showNotification(message, intent, title = tempTitle)
+//        else
+//            imageUrl.makeBitmapFromUrl({
+//                showNotification(NotificationType.FCM, message, intent, it, tempTitle)
+//            }) {
+//                showNotification(NotificationType.FCM, message, intent, title = tempTitle)
+//            }
+    }
+
+    /**
+     * 通知を表示
+     *
+     * @param type
+     * @param content
+     * @param intent
+     * @param largeIcon
+     * @param title
+     */
+    private fun showNotification(
+        content: String,
+        intent: Intent = Intent(applicationContext, SplashActivity::class.java),
+        largeIcon: Bitmap? = null,
+        title: String = applicationContext.getString(R.string.app_name)
+    ) {
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            createChannel()
+        showNotification(title, content, pendingIntent, largeIcon)
+    }
+
+    /**
+     * Channelの作成
+     *
+     * @param type
+     */
+    @TargetApi(Build.VERSION_CODES.O)
+    private fun createChannel() {
+        val channelId = "channelId"
+        val channelName = "channelName"
+        if (notificationManager.getNotificationChannel(channelId) == null)
+            notificationManager.createNotificationChannel( NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH).apply {
+                description = channelName
+            })
+    }
+
+    /**
+     * 通知の表示
+     *
+     * @param type
+     * @param title
+     * @param content
+     * @param pendingIntent
+     * @param largeIcon
+     */
+    private fun showNotification(title: String, content: String, pendingIntent: PendingIntent, largeIcon: Bitmap?) {
+        val notification = NotificationCompat.Builder(applicationContext, "channelId")
+            .setSmallIcon(R.drawable.ic_android_black_24dp)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setPriority(Notification.DEFAULT_ALL)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .apply {
+                largeIcon?.also {
+                    setLargeIcon(it)
+                }
             }
+            .setStyle(NotificationCompat.BigTextStyle().setBigContentTitle(title).bigText(content))
+            .build()
 
-            // データメッセージ
-            message.data?.let {
-                // データメッセージを処理
-                Log.w("push", "通知data")
-
-            }
-        }
+        notificationManager.notify(1, notification)
     }
 
     private fun showNotification(remoteMessage: RemoteMessage){
